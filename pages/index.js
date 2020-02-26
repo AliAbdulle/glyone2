b753ecf82d25d81150f91e2f136e6c590eed431a
AliAbdulle <alidhobey@gmail.com>
2020-02-25 20:26:29 -0600

    Create index.js to browse the web

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import './index.css';
+import Card from './Card';
+
+export default () => (
+    <div>
+        <header>
+
+        </header>
+        <div className="Grid">
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    </div>
+)
